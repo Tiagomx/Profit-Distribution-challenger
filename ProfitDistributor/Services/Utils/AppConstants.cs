8424246be4b3c23bdc3a28b516afb40b9884f17e
tiago.teixeira <tiago.teixeira@dellead.com>
2022-02-10 13:25:34 -0300

    Code Adjusts

--- ProfitDistributor/Services/Utils/AppConstants.cs
+++ ProfitDistributor/Services/Utils/AppConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int MONTHS_IN_YEAR = 12;
         public const decimal PESO_UM = 1;
-        public const string ESTAGIARIO = "ESTAGIARIO";
+        public const string ESTAGIARIO = "Estagiario";
         public const decimal MINIMUM_WAGE = 1000;
     }
 }
